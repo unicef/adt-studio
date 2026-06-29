f319bce90e5ac3411dcc759f39c2a3b85a738c28
zignaggo <ziggnago@gmail.com>
2026-06-29 10:01:40 -0300

    fix: remove loop attribute and create a test to validate

--- apps/adt-runtime/src/features/sign-language/components/SLVideo.tsx
+++ apps/adt-runtime/src/features/sign-language/components/SLVideo.tsx
@@ -153,7 +153,6 @@ export function SLVideo() {
           key={src}
           src={src}
           autoPlay
-          loop
           playsInline
           controls
           onLoadedMetadata={(e) => {
